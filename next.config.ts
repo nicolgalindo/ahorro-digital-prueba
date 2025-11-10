9a1570cbd11c3b8769de390a7573c87fcd226712
Nicol Maria Alejandra Galindo Mojica <nicomaria21@hotmail.com>
2025-11-10 02:08:52 -0500

    se revierten cambios

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,22 @@
 import type { NextConfig } from "next";
 
+// Make basePath conditional so local dev doesn't 404.
+// If NEXT_PUBLIC_BASE_PATH is set (for CI / GitHub Pages), we'll use it.
+const configuredBasePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   // Use static HTML export mode (replaces the old `next export` command).
   // This makes `next build` produce a static `out/` folder suitable for GitHub Pages.
-  // If you will publish under a repo path (https://<user>.github.io/<repo>),
-  // set `basePath` and `assetPrefix` to `'/<repo>'` and `'/<repo>/'` respectively.
   output: 'export',
-  // Configure basePath/assetPrefix since the site will be served from
-  // https://<user>.github.io/ahorro-digital-prueba
-  basePath: '/ahorro-digital-prueba',
-  assetPrefix: '/ahorro-digital-prueba/',
+  // Only set basePath/assetPrefix when NEXT_PUBLIC_BASE_PATH is defined.
+  ...(configuredBasePath
+    ? {
+        basePath: configuredBasePath,
+        assetPrefix: `${configuredBasePath}/`,
+      }
+    : {}),
 };
 
 export default nextConfig;
